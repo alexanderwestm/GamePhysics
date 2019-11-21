faa7bafdd61c3c12a05caa52ba6c9cb29a51ecee
Elzanaty <karimelzanaty98@gmail.com>
2019-11-21 09:39:05 -0500

    plugin stuff

---
+++ Project/Assets/Scripts/Plugins/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine;
+using System.Runtime.InteropServices;
+
+public static class Plugin
+{
+    [DllImport("Plugin")]
+    public static extern int InitFoo(int f_new = 0);
+
+    [DllImport("Plugin")]
+    public static extern int DoFoo(int bar = 0);
+
+    [DllImport("Plugin")]
+    public static extern int TermFoo();
+}
